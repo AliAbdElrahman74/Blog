efd0f9677fcf4866861736fb3cc233d33ba868ec
ali <aliabdelrhmanweka74@gmail.com>
2017-06-14 19:30:03 +0200

    add ui not complete

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,11 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( jquery.fancybox.css )
+Rails.application.config.assets.precompile += %w( style-responsive.css )
+Rails.application.config.assets.precompile += %w( components.css )
+Rails.application.config.assets.precompile += %w( custom.css )
+Rails.application.config.assets.precompile += %w( style.css )
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611163848) do
+ActiveRecord::Schema.define(version: 20170614160532) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170611163848) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.string   "string"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
